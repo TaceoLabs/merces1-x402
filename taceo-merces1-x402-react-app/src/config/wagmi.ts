9df21a39f61fc9203ffb6204589f5687f665795f
Fabian Gruber <fabian.gruber5@gmail.com>
2026-05-12 11:26:12 +0200

    fix: rm anvil from wagmi config in prod, add mainnet so that QR code login works

--- taceo-merces1-x402-react-app/src/config/wagmi.ts
+++ taceo-merces1-x402-react-app/src/config/wagmi.ts
@@ -1,13 +1,17 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
-import { baseSepolia, foundry } from "viem/chains";
+import { baseSepolia, foundry, mainnet } from "viem/chains";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const config = getDefaultConfig({
   appName: "Confidential x402 Demo",
   appDescription: "Demo of the confidential x402 payment scheme by Merces / TACEO",
   appUrl: process.env.NEXT_PUBLIC_APP_URL ?? "",
   projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? "demo",
-  chains: [foundry, baseSepolia],
+  // Need to include mainnet for the metamask app QR code to work, even though
+  // the app itself doesn't interact with mainnet.
+  chains: [isProduction ? mainnet : foundry, baseSepolia],
   ssr: true,
   wallets: [
     {
